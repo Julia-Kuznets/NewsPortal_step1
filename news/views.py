50618adff00174b13c2866ac09834bc43193b7a3
Name <2238812@mail.ru>
2024-01-11 01:58:43 +0300

    Выполнена часть работы из модуля D4

--- news/views.py
+++ news/views.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
+from .filters import PostFilter
 # Create your views here.
 
 class PostsList(ListView):
@@ -16,6 +17,19 @@ class PostsList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = PostFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
 
     def get_context_data(self, **kwargs):
         # С помощью super() мы обращаемся к родительским классам
@@ -28,6 +42,7 @@ class PostsList(ListView):
         # Добавим ещё одну пустую переменную,
         # чтобы на её примере рассмотреть работу ещё одного фильтра.
         context['next_post'] = None
+        context['filterset'] = self.filterset
         return context
 
 class PostDetail(DetailView):
