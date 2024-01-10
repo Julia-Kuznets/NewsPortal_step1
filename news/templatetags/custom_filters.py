2578dba227af02eed84f098bd631560ce9dab7bb
Name <2238812@mail.ru>
2024-01-10 03:07:16 +0300

    Модуль D3 завершен

---
+++ news/templatetags/custom_filters.py
@@ -0,0 +1,16 @@
+from django import template
+
+
+register = template.Library()
+
+TRASH_WORDS = ['душнила', 'пес']
+
+# Регистрируем наш фильтр под именем currency, чтоб Django понимал,
+# что это именно фильтр для шаблонов, а не простая функция.
+@register.filter()
+def censor(value):
+   for word in TRASH_WORDS:
+      value = value.replace(word[1:], '*' * len(word[1:]))
+   return value
+
+
